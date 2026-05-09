0e4681a97e418d378c3a2629fe5fd25b1c585493
Siddharth Gulati <sid441.sg@gmail.com>
2026-05-09 12:16:33 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const validOpen = ["(", "[", "{"];
+        const validClose = [")", "]", "}"];
+
+        const items = {
+            ")": "(",
+            "]": "[",
+            "}": "{",
+        };
+        if (s.length === 1) return false;
+
+        if (validClose.includes(s[0])) return false;
+
+        const tracker = [];
+        tracker.push(s[0]);
+
+        let j = 1;
+
+        while (j < s.length) {
+            if (validOpen.includes(s[j])) {
+                tracker.push(s[j]);
+            } else {
+                if (tracker[tracker.length - 1] !== items[s[j]]) {
+                    return false;
+                } else {
+                    tracker.pop();
+                }
+            }
+            j++;
+        }
+        return tracker.length === 0;
+    }
+}
