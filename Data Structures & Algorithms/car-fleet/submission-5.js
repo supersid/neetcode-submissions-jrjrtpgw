92b07d04fb94d1f2273aaeac36ebc736dd275a1d
Siddharth Gulati <sid441.sg@gmail.com>
2026-05-10 09:18:02 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const combined = position.map((v, k) => {
+            return { position: v, speed: speed[k] };
+        });
+        const sorted = combined.sort((a, b) => b.position - a.position);
+        const count = [];
+        for (let val of sorted) {
+            const currTime = (target - val.position) / val.speed;
+            if (count.length === 0 || currTime > count[count.length - 1]) {
+                count.push(currTime);
+            }
+        }
+        return count.length;
+    }
+}
