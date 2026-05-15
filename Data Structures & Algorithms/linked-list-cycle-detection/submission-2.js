e067bb7dae76bfe84d00f3c0fcd1b94e9deb3da5
Siddharth Gulati <sid441.sg@gmail.com>
2026-05-14 22:25:30 -0400

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let p1 = head;
+        let p2 = head;
+
+        while (p2 !== null && p2.next !== null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 === p2) return true;
+        }
+        return false;
+    }
+}
